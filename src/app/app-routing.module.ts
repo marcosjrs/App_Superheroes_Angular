83e484895c01111a659e8ef4dbe9509d3a668a29
marcosjrs <marcosjrs80@gmail.com>
2018-10-29 11:22:23 +0100

    Mapeo de rutas a componentes y se añaden los routerLinks en el navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { HeroesComponent } from './components/heroes/heroes.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'heroes', component: HeroesComponent },
   { path: '**', redirectTo: 'home' }
 ]
 
